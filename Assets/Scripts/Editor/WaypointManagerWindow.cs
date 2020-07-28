121154c36ddc652b10cb28b208a6a89d8e285207
vladstheawesome <vmabhena@gmail.com>
2020-07-28 17:13:41 +0100

    Waypoint Manager Window

--- Assets/Scripts/Editor/WaypointManagerWindow.cs
+++ Assets/Scripts/Editor/WaypointManagerWindow.cs
@@ -1,21 +1,72 @@
-﻿using System.Collections;
+﻿using CarGame.WaypointLogic;
+using System;
+using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 namespace CarGame
 {
-    public class WapointManagerWindow : MonoBehaviour
+    public class WaypointManagerWindow : EditorWindow
     {
-        // Start is called before the first frame update
-        void Start()
+        [MenuItem("Tools/Waypoint Editor")]
+        public static void Open()
         {
-
+            GetWindow<WaypointManagerWindow>();
         }
 
-        // Update is called once per frame
-        void Update()
-        {
+        public Transform waypointRoot; // parent for our waypoints
 
+        private void OnGUI()
+        {
+            SerializedObject obj = new SerializedObject(this);
+
+            EditorGUILayout.PropertyField(obj.FindProperty("waypointRoot"));
+
+            if (waypointRoot == null)
+            {
+                EditorGUILayout.HelpBox("Root transform must be selected. Assign a ROOT TRANSFORM", MessageType.Warning);
+            }
+            else
+            {
+                EditorGUILayout.BeginVertical("box");
+                DrawButtons();
+                EditorGUILayout.EndVertical();
+            }
+
+            obj.ApplyModifiedProperties();
+        }
+
+        void DrawButtons()
+        {
+            // Create a button and a method for adding a waypoint
+            if(GUILayout.Button("Create Waypoint"))
+            {
+                CreateWayPoint();
+            }
+        }
+
+        void CreateWayPoint()
+        {
+            GameObject waypointObject = new GameObject("Waypoint " + waypointRoot.childCount, typeof(Waypoint));
+
+            // set this way point as a child of our waypoint root
+            waypointRoot.transform.SetParent(waypointRoot, false);
+
+            Waypoint waypoint = waypointObject.GetComponent<Waypoint>();
+
+            // We want our child waypoints to link to each other
+            if(waypointRoot.childCount > 1)
+            {
+                waypoint.previousWaypoint = waypointRoot.GetChild(waypointRoot.childCount - 2).GetComponent<Waypoint>();
+                waypoint.previousWaypoint.nextWaypoint = waypoint;
+                // Place the waypoint at the last position
+                waypoint.transform.position = waypoint.previousWaypoint.transform.position;
+                // Set the waypoints to face in same direction and previous waypoint
+                waypoint.transform.forward = waypoint.previousWaypoint.transform.forward;
+            }
+
+            Selection.activeGameObject = waypoint.gameObject;
         }
     }
 }
--- Assets/Scripts/WaypointLogic/Waypoint.cs
+++ Assets/Scripts/WaypointLogic/Waypoint.cs
@@ -14,7 +14,14 @@ namespace CarGame.WaypointLogic
 
         public Vector3 GetPosition()
         {
+            // Return a random point based on the waypoint width
+            // To give some degree of freedom for the pedestrains to move between
+            // when they are moving towards a waypoint
 
+            Vector3 minBound = transform.position + transform.right * width / 2f;
+            Vector3 maxBound = transform.position - transform.right * width / 2f;
+
+            return Vector3.Lerp(minBound, maxBound, Random.Range(0f, 1f));
         }
     }
 }
