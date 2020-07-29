b4f12e2ab0de9be794b38d431d4feb0e069c446e
vladstheawesome <vmabhena@gmail.com>
2020-07-29 13:16:59 +0100

    Spawning Pedestrians

---
+++ Assets/Scripts/PedestrianSpawner.cs
@@ -0,0 +1,33 @@
+﻿using CarGame.WaypointLogic;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PedestrianSpawner : MonoBehaviour
+{
+    public GameObject pedestrianPrefab;
+    public int pedestriansToSpawn;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(Spawn());
+    }
+
+    IEnumerator Spawn()
+    {
+        int count = 0;
+        while (count < pedestriansToSpawn)
+        {
+            GameObject obj = Instantiate(pedestrianPrefab);
+            Transform child = transform.GetChild(UnityEngine.Random.Range(0, transform.childCount - 1));
+            obj.GetComponent<WaypointNavigator>().currentWaypoint = child.GetComponent<Waypoint>();
+            obj.transform.position = child.position;
+
+            yield return new WaitForEndOfFrame();
+
+            count++;
+        }
+    }
+}
--- Assets/Scripts/WaypointLogic/WaypointNavigator.cs
+++ Assets/Scripts/WaypointLogic/WaypointNavigator.cs
@@ -8,17 +8,27 @@ namespace CarGame.WaypointLogic
     public class WaypointNavigator : MonoBehaviour
     {
         PedestrianController controller;
+        public Waypoint currentWaypoint;
+
+        private void Awake()
+        {
+            controller = GetComponent<PedestrianController>();
+        }
 
         // Start is called before the first frame update
         void Start()
         {
-
+            controller.SetDestination(currentWaypoint.GetPosition());
         }
 
         // Update is called once per frame
         void Update()
         {
-
+            if (controller.reachedDestination)
+            {
+                currentWaypoint = currentWaypoint.nextWaypoint;
+                controller.SetDestination(currentWaypoint.GetPosition());
+            }
         }
     }
 }
